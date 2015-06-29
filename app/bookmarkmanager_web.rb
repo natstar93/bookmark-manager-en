0b82e3c26f642126dae207ebb829631fb9ccafbb
Jonathan Ng <jonngkh@hotmail.com>
2015-06-29 18:55:12 +0100

    passed first test

--- app/bookmarkmanager_web.rb
+++ app/bookmarkmanager_web.rb
@@ -1,12 +1,14 @@
 require 'sinatra/base'
 
 class BookmarkManager < Sinatra::Base
+  set :views, proc { File.join(root, '..', 'views') }
+
   get '/' do
     'Hello BookmarkManager!'
   end
 
-  get'/links' do
-    'Makers Academy'
+  get '/links' do
+    erb :links
   end
 
   # start the server if ruby file executed directly
