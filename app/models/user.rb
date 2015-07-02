8383b839c582e1cdd0eddb3555959d9432cb1c82
Elin <elinnet@gmail.com>
2015-07-02 17:24:50 +0100

    updated for validation of uniqueness of user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User
 
   validates_confirmation_of :password
   #validates_presence_of :email
+  validates_uniqueness_of(:email)
 
   property :id, Serial
   property :email, String, required: true
