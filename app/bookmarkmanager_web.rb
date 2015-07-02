e9f1f658f424071d08f44c59bb7eb72b89d7dce0
Natalie Akam <natalie.akam@gmail.com>
2015-07-02 12:55:49 +0100

    Chages user to instance variable

--- app/bookmarkmanager_web.rb
+++ app/bookmarkmanager_web.rb
@@ -58,13 +58,13 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/users' do
-    user = User.create(email: params[:email],
+    @user = User.create(email: params[:email],
       password: params[:password], password_confirmation: params[:password_confirmation])
     session[:user_id] = user.id
     
 
-    if user.save 
-        session[:user_id] = user.id
+    if @user.save 
+        session[:user_id] = @user.id
         redirect to('/links')
     else
         flash.now[:notice]="Password and confirmation password do not match"
